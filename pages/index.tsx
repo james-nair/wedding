b0fa51fcd950711c0ec471804594582042089252
James Nair <vanairngo2@hotmail.com>
2022-11-29 20:16:46 +1000

    added toasts

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,14 +3,12 @@ import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import Link from "next/link";
 import Loader from "../components/Loader";
+import toast from "react-hot-toast";
 
 export default function Home() {
   return (
     <div>
-      <Loader show />
-      <Link href={{ pathname: "./[username]", query: { username: "myname" } }}>
-        My Profile
-      </Link>
+      <button onClick={() => toast.success("hello toast!")}>Toast</button>
     </div>
   );
 }
