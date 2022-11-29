53bca95bae156b7a90de79042d2178e2e6e9faac
James Nair <vanairngo2@hotmail.com>
2022-11-29 21:43:16 +1000

    added some user auth flow

--- pages/enter.tsx
+++ pages/enter.tsx
@@ -1,11 +1,49 @@
+import { signInWithPopup, signOut } from "firebase/auth";
+import { auth, googleAuthProvider } from "../lib/firebase";
+
 type enterProps = {};
 
 const EnterPage = (props: enterProps) => {
+  const user = null;
+  const username = null;
   return (
     <main>
-      <h1>Sign Up</h1>
+      {user ? (
+        !username ? (
+          <UsernameForm />
+        ) : (
+          <SignOutButton />
+        )
+      ) : (
+        <SignInButton />
+      )}
     </main>
   );
+};
+
+const SignInButton = () => {
+  const signinWithGoogle = async () => {
+    try {
+      let user = await signInWithPopup(auth, googleAuthProvider);
+      console.log(user);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <button className="btn-google" onClick={signinWithGoogle}>
+      Sign in with google
+    </button>
+  );
+};
+
+const SignOutButton = () => {
+  return <button onClick={() => signOut(auth)}>Sign Out</button>;
+};
+
+const UsernameForm = () => {
+  return <></>;
 };
 
 export default EnterPage;
