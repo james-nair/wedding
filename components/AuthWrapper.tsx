fe927f8003c3ea68f59496aabbfae695bb87898f
James Nair <vanairngo2@hotmail.com>
2022-12-04 02:34:30 +1000

    update auth wrapper and styling

--- components/AuthWrapper.tsx
+++ components/AuthWrapper.tsx
@@ -1,14 +1,32 @@
+import { useMediaQuery } from "@mui/material";
 import Link from "next/link";
 import { ReactNode } from "react";
 import { useAuth } from "../lib/UserAuth";
+import { LoginForm } from "./admin/LoginForm";
 
 type Props = {
   children: ReactNode;
 };
 export const AuthWrapper = (props: Props) => {
-  const { username } = useAuth();
+  const { username, user } = useAuth();
+  const isSm = useMediaQuery("md");
 
-  if (username) return <>{props.children}</>;
+  if (user) return <>{props.children}</>;
 
-  return <Link href="/">You're not suppose to be here</Link>;
+  if (user === null)
+    return (
+      <div
+        style={{
+          marginLeft: "auto",
+          marginRight: "auto",
+          padding: "3rem 0",
+          maxWidth: isSm ? "70%" : "40%",
+        }}
+      >
+        <h3>Login To Admin Portal</h3>
+        <LoginForm />
+      </div>
+    );
+
+  return <></>;
 };
--- pages/admin/index.tsx
+++ pages/admin/index.tsx
@@ -1,5 +1,30 @@
+import { styled } from "@mui/material";
+import { GuestTable } from "../../components/admin/GuestTable";
+import { Guest } from "../../components/admin/types";
 import { AuthWrapper } from "../../components/AuthWrapper";
 import { useAuth } from "../../lib/UserAuth";
+
+interface ComponentProps {
+  isPortrait?: boolean;
+  isSmall?: boolean;
+}
+const Section = styled("section", {
+  shouldForwardProp: (prop) => prop !== "isPortrait",
+})<ComponentProps>({
+  height: "100%",
+  overflow: "hidden",
+  position: "relative",
+  backgroundColor: "#EFEBE9",
+  transition: "background 1s ease-in",
+});
+
+const Layout = styled("div")<ComponentProps>({
+  width: "100%",
+  color: "black",
+  textAlign: "center",
+  // marginTop: "3.5%",
+  animation: "fadein 2.5s",
+});
 
 type AdminPageProps = {};
 
@@ -7,10 +32,24 @@ const AdminPage = (props: AdminPageProps) => {
   const { username, user } = useAuth();
 
   return (
-    <main>
+    <main style={{ height: "100%" }}>
       <AuthWrapper>
-        Hello, my username is {username}
-        <div style={{ width: "100%" }}>{JSON.stringify(user)}</div>
+        <Section>
+          <Layout>
+            <div
+              style={{
+                marginLeft: "auto",
+                marginRight: "auto",
+                padding: "3rem 0",
+                maxWidth: "70%",
+              }}
+            >
+              <GuestTable label={"Test"} guestList={[new Guest()]} />
+              {/* Hello, my username is {username}
+        <div style={{ width: "100%" }}>{JSON.stringify(user)}</div> */}
+            </div>
+          </Layout>
+        </Section>
       </AuthWrapper>
     </main>
   );
