d306edb8ae8a5de7a3600f43fa02f35126b60e13
James Nair <vanairngo2@hotmail.com>
2022-12-06 01:47:26 +1000

    added Timline component

---
+++ components/CustomTimeline.tsx
@@ -0,0 +1,58 @@
+import {
+  Timeline,
+  TimelineConnector,
+  TimelineContent,
+  TimelineDot,
+  TimelineItem,
+  TimelineOppositeContent,
+  TimelineSeparator,
+} from "@mui/lab";
+import { Typography } from "@mui/material";
+import { ReactNode } from "react";
+
+import { useMediaQuery } from "../lib/useMediaQuery";
+
+export type TimelineItem = {
+  title: string;
+  subTitle?: string;
+  time: Date;
+  icon: ReactNode;
+};
+
+type Props = {
+  items: TimelineItem[];
+};
+
+const isEven = (num: number) => num % 2 == 0;
+
+export const CustomTimeline = (props: Props) => {
+  const isSm = useMediaQuery("md");
+  return (
+    <Timeline position="alternate">
+      {props.items.map((item, i) => (
+        <TimelineItem key={`timeline_${item.title}_${i}`}>
+          <TimelineOppositeContent
+            sx={{ m: isSm ? "auto 0 0" : "auto 0" }}
+            {...(isEven(i) && { align: "right" })}
+            variant="body2"
+            color="text.secondary"
+            // sx={{ display: "flex", alignItems: "flex-end" }}
+          >
+            {item.time.toLocaleTimeString("en-AU", { timeStyle: "short" })}{" "}
+            {item.time.toLocaleDateString("en-AU")}
+          </TimelineOppositeContent>
+          <TimelineSeparator>
+            <TimelineConnector />
+            <TimelineDot>{item.icon}</TimelineDot>
+          </TimelineSeparator>
+          <TimelineContent sx={{ m: isSm ? "auto 0 0" : "auto 0" }}>
+            <Typography variant={isSm ? "h6" : "h5"} component="span">
+              {item.title}
+            </Typography>
+            {item.subTitle && <Typography>{item.subTitle}</Typography>}
+          </TimelineContent>
+        </TimelineItem>
+      ))}
+    </Timeline>
+  );
+};
