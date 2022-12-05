5cde22bebc154f7f8e91963dfe0dde2c0894170b
James Nair <vanairngo2@hotmail.com>
2022-12-06 06:07:21 +1000

    Added about us component

---
+++ components/AboutUs.tsx
@@ -0,0 +1,38 @@
+import { styled } from "@mui/material";
+import { useConfig } from "../lib/useConfig";
+import { BACKGROUND_COLOR } from "../styles/constant";
+import { Info } from "./Info";
+
+interface ComponentProps {
+  isPortrait?: boolean;
+  isSmall?: boolean;
+}
+const Section = styled("section", {
+  shouldForwardProp: (prop) => prop !== "isPortrait",
+})<ComponentProps>({
+  height: "fit-content",
+  overflow: "hidden",
+  position: "relative",
+  backgroundColor: BACKGROUND_COLOR,
+  textAlign: "center",
+  padding: "1rem 0 3rem 0",
+});
+
+const Container = styled("div", {
+  shouldForwardProp: (prop) => prop !== "isSmall" && prop !== "isPortrait",
+})<ComponentProps>(({ isSmall }) => ({
+  maxWidth: isSmall ? "95%" : "80%",
+  margin: "0 auto",
+}));
+export const AboutUs = () => {
+  const config = useConfig();
+
+  return (
+    <Section>
+      <Container>
+        <Info person={config.groom} title="The Groom" left />
+        <Info person={config.bride} title="The Bride" right />
+      </Container>
+    </Section>
+  );
+};
---
+++ components/Info.tsx
@@ -0,0 +1,73 @@
+import { Grid, Typography } from "@mui/material";
+import { Person } from "../lib/useConfig";
+import { useMediaQuery } from "../lib/useMediaQuery";
+import { CursiveFont } from "../styles/constant";
+
+type ImageLocation =
+  | { left: true; right?: never }
+  | { left?: never; right: true };
+type Props = {
+  title: string;
+  person: Person;
+} & ImageLocation;
+export const Info = (props: Props) => {
+  const isSmall = useMediaQuery("md");
+  //   if (props.left) {
+  //     return (
+  //       <Grid container spacing={3}>
+  //         <Grid item xs={12} md={4}>
+  //           <img
+  //             src={props.person.image?.src}
+  //             style={{
+  //               width: "100%",
+
+  //               objectFit: "contain",
+  //               borderRadius: "50%",
+  //             }}
+  //           />
+  //         </Grid>
+  //         <Grid item xs={12} md={8}>
+  //           <h4>Hello</h4>
+  //           <p>{props.person.name}</p>
+  //         </Grid>
+  //       </Grid>
+  //     );
+  //   } else
+  return (
+    <Grid
+      container
+      spacing={3}
+      sx={{
+        ...(props.right && { flexDirection: "row-reverse" }),
+        display: "flex",
+        alignItems: "center",
+      }}
+    >
+      <Grid item xs={12} md={4}>
+        <img
+          src={props.person.image?.src}
+          style={{
+            width: "100%",
+
+            objectFit: "contain",
+            borderRadius: "50%",
+          }}
+        />
+      </Grid>
+      <Grid item xs={12} md={8}>
+        <div
+          style={{
+            // maxWidth: "70%",
+            ...(isSmall && { paddingBottom: "5rem" }),
+            ...(!isSmall && { textAlign: props.left ? "left" : "right" }),
+          }}
+        >
+          <Typography variant="h4">{props.title}</Typography>
+          <Typography variant="h3" className={CursiveFont.className}>
+            {props.person.name}
+          </Typography>
+        </div>
+      </Grid>
+    </Grid>
+  );
+};
