33995269b6541d06a696a38b26399781cac042f8
James Nair <vanairngo2@hotmail.com>
2022-12-07 01:01:52 +1000

    use QuiltedImage in Gallery

--- components/Gallery.tsx
+++ components/Gallery.tsx
@@ -1,14 +1,22 @@
 import { ImageList, ImageListItem, styled } from "@mui/material";
+import { StaticImageData } from "next/image";
 import { useRef } from "react";
-import { useConfig } from "../lib/useConfig";
+import { MyConfig, useConfig } from "../lib/useConfig";
 import { useIsPortrait } from "../lib/useIsPortrait";
+import { useMediaQuery } from "../lib/useMediaQuery";
 import useOnScreen from "../lib/useOnScreen";
 import {
   BACKGROUND_COLOR,
   BACKGROUND_TRANSITION,
   Header,
 } from "../styles/constant";
-
+import { ImageDisplay, QuiltedImageList } from "./QuiltedImageList";
+import Image1 from "../assets/PAVAnh_Prewedding_C1_4V0A9260-L.jpg";
+import Image2 from "../assets/PAVAnh_Prewedding_C1_4V0A9387-L.jpg";
+import Image3 from "../assets/PAVAnh_Prewedding_C1_4V0A9332-L.jpg";
+import Image4 from "../assets/PAVAnh_Prewedding_C1_4V0A9292-L.jpg";
+import Image5 from "../assets/PAVAnh_Prewedding_C1_4V0A9278-L.jpg";
+import Image6 from "../assets/PAVAnh_Prewedding_C1_4V0A9472.jpg";
 interface ComponentProps {
   isPortrait?: boolean;
 }
@@ -16,6 +24,7 @@ interface ComponentProps {
 export const Gallery = () => {
   const config = useConfig();
   const isPortrait = useIsPortrait();
+  const isSmall = useMediaQuery("md");
   const ref = useRef<HTMLSelectElement>(null);
   const onScreen = useOnScreen<HTMLDivElement>(ref, "-125px");
 
@@ -23,14 +32,14 @@ export const Gallery = () => {
     <section
       ref={ref}
       style={{
-        height: "fit-content",
+        height: "",
         background: BACKGROUND_COLOR,
         overflow: "hidden",
         position: "relative",
       }}
     >
       <Header title="YoooHoooo" />
-      <ImageList variant="woven" cols={4} gap={2}>
+      {/* <ImageList variant="woven" cols={4} gap={2}>
         {config.galleryImages.map((image) => (
           <ImageListItem key={`image_${image.src}`}>
             <img
@@ -39,7 +48,50 @@ export const Gallery = () => {
             />
           </ImageListItem>
         ))}
-      </ImageList>
+      </ImageList> */}
+      <QuiltedImageList
+        sx={{ width: "100%", height: "100%" }}
+        cols={6}
+        data={ItemData()}
+        // rowHeight={10}
+      />
     </section>
   );
+};
+
+const ItemData = (): ImageDisplay[] => {
+  const images = MyConfig.galleryImages;
+
+  return [
+    {
+      src: Image1.src,
+      cols: 2,
+      rows: 6,
+    },
+    {
+      src: Image2.src,
+      cols: 1,
+      rows: 3,
+    },
+    {
+      src: Image3.src,
+      cols: 1,
+      rows: 3,
+    },
+    {
+      src: Image4.src,
+      cols: 2,
+      rows: 4,
+    },
+    {
+      src: Image5.src,
+      cols: 2,
+      rows: 3,
+    },
+    {
+      src: Image6.src,
+      cols: 2,
+      rows: 2,
+    },
+  ];
 };
---
+++ components/QuiltedImageList.tsx
@@ -0,0 +1,45 @@
+import { ImageList, ImageListItem, SxProps, Theme } from "@mui/material";
+import Image from "next/image";
+
+export type ImageDisplay = {
+  src: string;
+  cols: number;
+  rows: number;
+};
+
+function srcset(image: string, size: number, rows = 1, cols = 1) {
+  return {
+    src: `${image}?w=${size * cols}&h=${size * rows}&fit=crop&auto=format`,
+    srcSet: `${image}?w=${size * cols}&h=${
+      size * rows
+    }&fit=crop&auto=format&dpr=2 2x`,
+  };
+}
+
+type Props = {
+  cols: number;
+  data: ImageDisplay[];
+  //   rowHeight: number;
+  sx?: SxProps<Theme>;
+};
+export const QuiltedImageList = (props: Props) => {
+  return (
+    <ImageList
+      cols={6}
+      variant="quilted"
+      //   rowHeight={props.rowHeight}
+      sx={props.sx}
+    >
+      {props.data.map((item) => (
+        <ImageListItem key={item.src} cols={item.cols} rows={item.rows}>
+          {/* <img {...srcset(item.src, props.rowHeight, item.rows, item.cols)} /> */}
+          {/* <Image
+            alt={`quilted-image-${item.src}`}
+            {...srcset(item.src, props.rowHeight, item.rows, item.cols)}
+          /> */}
+          <img src={item.src} />
+        </ImageListItem>
+      ))}
+    </ImageList>
+  );
+};
